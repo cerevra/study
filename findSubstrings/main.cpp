7292944eaacd581f2c1c68d7158bc7d39ceaf0c9
klevanets <cerevra@gmail.com>
2014-11-07 16:29:59 +0300

    упрощение

--- findSubstrings/main.cpp
+++ findSubstrings/main.cpp
@@ -12,20 +12,16 @@ VecInt findKMP(VecInt& prefix_func, const string& str, unsigned int subLen){
     prefix_func.resize(len);
     prefix_func[0] = 0;
 
-    for (int i = 1; i < len; ++i){
-        int maybeIdx = i - 1;
-        int maybe    = prefix_func[maybeIdx];
+    for (int idx = 1; idx < len; ++idx){
+        int maybe = prefix_func[idx - 1];
 
-        while (maybe && str[i] != str[maybe]){
-            maybeIdx = prefix_func[maybeIdx] - 1;
-            maybe    = prefix_func[maybeIdx];
-        }
+        while (maybe && str[idx] != str[maybe])
+            maybe = prefix_func[maybe - 1];
 
         if (maybe)
-            prefix_func[i] = maybe + 1;
-        else {
-            prefix_func[i] = str[i] == str[maybe] ? 1 : 0;
-        }
+            prefix_func[idx] = maybe + 1;
+        else
+            prefix_func[idx] = str[idx] == str[maybe] ? 1 : 0;
     }
 
     VecInt result;
