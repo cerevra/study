e13940d02fa7a9decdeca37f9762936de0b7501d
klevanets <cerevra@gmail.com>
2015-01-13 12:25:22 +0300

    refactoring

--- findSubstrings/main.cpp
+++ findSubstrings/main.cpp
@@ -5,8 +5,13 @@ using namespace std;
 
 typedef vector<unsigned int> VecInt;
 
-VecInt findKMP(VecInt& prefix_func, const string& str, unsigned int subLen){
-    int len = str.size();
+VecInt findKMP(VecInt& prefix_func, const string& str, const string& sub){
+    string prepairedString;
+    prepairedString.append   (sub);
+    prepairedString.push_back(',');
+    prepairedString.append   (str);
+
+    int len = prepairedString.size();
 
     prefix_func.clear();
     prefix_func.resize(len);
@@ -15,19 +20,19 @@ VecInt findKMP(VecInt& prefix_func, const string& str, unsigned int subLen){
     for (int idx = 1; idx < len; ++idx){
         int maybe = prefix_func[idx - 1];
 
-        while (maybe && str[idx] != str[maybe])
+        while (maybe && prepairedString[idx] != prepairedString[maybe])
             maybe = prefix_func[maybe - 1];
 
         if (maybe)
             prefix_func[idx] = maybe + 1;
         else
-            prefix_func[idx] = str[idx] == str[maybe] ? 1 : 0;
+            prefix_func[idx] = prepairedString[idx] == prepairedString[maybe] ? 1 : 0;
     }
 
     VecInt result;
-    for (int i = subLen; i < len; ++i){
-        if (prefix_func[i] == subLen)
-            result.push_back(i - 2*subLen); // 2: 1) искусственно увеличенная строка (+$) 2) число указывает на конец (поэтому не надо вычитать 1 ($))
+    for (int i = sub.size(); i < len; ++i){
+        if (prefix_func[i] == sub.size())
+            result.push_back(i - 2*sub.size()); // 2: 1) искусственно увеличенная строка (+$) 2) число указывает на конец (поэтому не надо вычитать 1 ($))
     }
 
     return result;
@@ -43,21 +48,14 @@ int main()
         cout << "Enter Substring" << endl;
         cin  >> sub;
 
-        unsigned int subLen = sub.size();
-
-        if (subLen > str.size()){
+        if (sub.size() > str.size()){
             cout << "Sub if bigger then string" << endl;
-            return 0;
+            continue;
         }
-
-        string prepairedString;
-        prepairedString.append   (sub);
-        prepairedString.push_back('$');
-        prepairedString.append   (str);
 
         VecInt prefixes;
 
-        const VecInt& result = findKMP(prefixes, prepairedString, subLen);
+        const VecInt& result = findKMP(prefixes, str, sub);
         cout << "Results:" << endl;
         for (auto i = begin(result); i < end(result); ++i)
             cout << *i << endl;
